4fbf1a33f375620bc4d325f93f34740bf0273e73
Marcon Willian <marcon@mh4sh.dev>
2021-08-29 17:18:27 -0300

    chore ✨: Update to start use SOLID

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 
-import { CategoriesRepositories } from '../repositories/CategoriesRepositories';
+import { CategoriesRepository } from '../repositories/CategoriesRepository';
+import { CreateCategoryService } from '../services/CreateCategoryService';
 
 const categoriesRoutes = Router();
-const categoriesRepository = new CategoriesRepositories();
+const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.get('/', (request, response) => {
   const allCategories = categoriesRepository.list();
@@ -13,14 +14,8 @@ categoriesRoutes.get('/', (request, response) => {
 
 categoriesRoutes.post('/', (request, response) => {
   const { name, description } = request.body;
-
-  const categoryAlreadyExists = categoriesRepository.findByName(name);
-
-  if (categoryAlreadyExists) {
-    return response.status(400).json({ error: 'Category already exist' });
-  }
-
-  categoriesRepository.create({ name, description });
+  const createCategoryService = new CreateCategoryService(categoriesRepository);
+  createCategoryService.execute({ name, description });
 
   return response.status(201).send();
 });
--- src/server.ts
+++ src/server.ts
@@ -8,12 +8,6 @@ app.use(express.json());
 
 app.use('/categories', categoriesRoutes);
 
-app.post('/courses', (request, response) => {
-  const { name } = request.body;
-
-  return response.json({ name });
-});
-
 app.listen(3333, () => {
   console.log('Server is running!');
 });
